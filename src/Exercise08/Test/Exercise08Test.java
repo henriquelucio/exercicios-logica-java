20aac466950c97f57ff25bbd0dd1dd8f00140019
Henrique Barcelos <henriquelucio95@hotmail.com>
2026-07-07 19:09:05 -0300

    Refactor: Exercise08 with loop for sorting numbers

--- src/Exercise08/Test/Exercise08Test.java
+++ src/Exercise08/Test/Exercise08Test.java
@@ -7,62 +7,79 @@ import java.util.stream.IntStream;
 
 public class Exercise08Test {
     public static void main(String[] args) {
-//        int[] array = new int[3];
-//        int[] sortedArray = new int[3];
-        int value1;
-        int value2;
-        int value3;
-        int maior;
-        int meio;
-        int menor;
-
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Provide value 1: ");
-        value1 = input.nextInt();
-        System.out.println("Provide value 2: ");
-        value2 = input.nextInt();
-        System.out.println("Provide value 3: ");
-        value3 = input.nextInt();
+        int[] array = new int[3];
+        int[] sortedArray = new int[3];
+        int[] countArray = new int[3];
 
-        if (value3 > value1 && value3 > value2){
-            maior = value3;
-            if(value2 > value1){
-                meio = value2;
-                menor = value1;
-            }else{
-                meio = value1;
-                menor = value2;
-            }
-        }else if(value2 > value1){
-            maior = value2;
-            if(value1 > value3){
-                meio = value1;
-                menor = value3;
-            }else{
-                meio = value3;
-                menor = value1;
-            }
-        }else{
-            maior = value1;
-            if(value2 > value3){
-                meio = value2;
-                menor = value3;
-            }else{
-                meio = value3;
-                menor = value2;
-            }
+//        int value1;
+//        int value2;
+//        int value3;
+//        int maior;
+//        int meio;
+//        int menor;
+
+//        System.out.println("Provide value 1: ");
+//        value1 = input.nextInt();
+//        System.out.println("Provide value 2: ");
+//        value2 = input.nextInt();
+//        System.out.println("Provide value 3: ");
+//        value3 = input.nextInt();
+//
+//        if (value3 > value1 && value3 > value2){
+//            maior = value3;
+//            if(value2 > value1){
+//                meio = value2;
+//                menor = value1;
+//            }else{
+//                meio = value1;
+//                menor = value2;
+//            }
+//        }else if(value2 > value1){
+//            maior = value2;
+//            if(value1 > value3){
+//                meio = value1;
+//                menor = value3;
+//            }else{
+//                meio = value3;
+//                menor = value1;
+//            }
+//        }else{
+//            maior = value1;
+//            if(value2 > value3){
+//                meio = value2;
+//                menor = value3;
+//            }else{
+//                meio = value3;
+//                menor = value2;
+//            }
+//        }
+//
+//        System.out.println(maior);
+//        System.out.println(meio);
+//        System.out.println(menor);
+
+
+        for(int i = 0; i < array.length; i++){
+            System.out.println("Provide value: ");
+            array[i] = input.nextInt();
         }
 
-        System.out.println(maior);
-        System.out.println(meio);
-        System.out.println(menor);
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array.length; j++){
+                if(array[j] > array[i]){
+                    countArray[i]++;
+                }
 
-        
-//        for(int i = 0; i < array.length; i++){
-//            System.out.println("Provide value: ");
-//            array[i] = input.nextInt();
-//        }
+            }
+            sortedArray[countArray[i]] = array[i];
+        }
+
+        System.out.println("Sorted array: ");
+        for(int i = 0; i < sortedArray.length; i++){
+            System.out.println(sortedArray[i]);
+        }
 
         //array = IntStream.of(array).sorted().toArray();
 //        for (int i = 0; i < array.length; i++){
