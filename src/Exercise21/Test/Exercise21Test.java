7604eddd64e1393445e19555766aed66dd04924e
Henrique Barcelos <henriquelucio95@hotmail.com>
2026-07-14 21:41:15 -0300

    Feat: Exercise21

---
+++ src/Exercise21/Test/Exercise21Test.java
@@ -0,0 +1,9 @@
+package Exercise21.Test;
+
+// 21 - Faça um algoritmo que mostre um valor aleatório entre 0 e 100.
+
+public class Exercise21Test {
+    public static void main(String[] args){
+        System.out.println(Math.round(Math.random()*100));
+    }
+}
